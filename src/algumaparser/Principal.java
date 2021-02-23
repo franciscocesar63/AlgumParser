3c814c256f4990488ed343969320ca40f617f216
Cesar <franciscocesar888@gmail.com>
2021-02-23 16:36:25 -0300

    AlgumParser

---
+++ src/algumaparser/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algumaparser;
+
+import br.com.fafic.analiselexica.AlgumaLexico;
+
+/**
+ *
+ * @author franc
+ */
+public class Principal {
+
+    public static void main(String args[]) {
+        AlgumaLexico lex = new AlgumaLexico(args[0]);
+        AlgumaParser parser = new AlgumaParser(lex);
+        parser.programa();
+    }
+}
